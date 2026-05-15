c98284bf96e70051b96ae002172dbd77a1571b60
samir martinez acosta <samiresteyber0511@hotmail.com>
2026-05-14 22:40:54 -0500

    test de esenario de ruptura 1 - findAllInvoive

--- src/main/java/com/sistema/facturacion2/service/InvoiceService.java
+++ src/main/java/com/sistema/facturacion2/service/InvoiceService.java
@@ -177,14 +177,8 @@ public class InvoiceService {
         List<InvoiceDTO> content = pageResult.getContent()
                 .stream()
                 .map(invoiceMapper::toDto).toList();
-        return new PageResponseDTO<>(
-                content,
-                pageResult.getNumber(),
-                pageResult.getSize(),
-                pageResult.getTotalElements(),
-                pageResult.getTotalPages(),
-                pageResult.isLast()
-        );
+        // ROTO INTENCIONALMENTE PARA PRUEBA DE PIPELINE
+        return null;
     }
     /**
      * Busca una factura por su identificador único, cargando sus ítems y usuario en la misma consulta.
